5b9f4fab8b45dfbe40c421be0f3c9c9a17a02f35
Руслан Залилов <kingsbounty99@gmail.com>
2023-09-30 01:27:36 +0500

    [CSHW-2]: Switch to Xunit, added simple test

---
+++ test/Ozon.Route256.Practice.Test/RegionsTest.cs
@@ -0,0 +1,19 @@
+﻿using Ozon.Route256.Practice.OrdersService.Services;
+
+namespace Ozon.Route256.Practice.Test;
+
+public class RegionsTest
+{
+    private readonly RegionService _regionService;
+    
+    public RegionsTest()
+    {
+        _regionService = new RegionService();
+    }
+    
+    [Fact]
+    public void ShouldFindRURegion()
+    {
+        Assert.True(_regionService.HasRegion("RU"));
+    }
+}
